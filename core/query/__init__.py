f1be6faa5f74a44b174070f0b55c88d267f1844f
Yjh-Rking <ying_register@163.com>
2026-03-10 10:08:46 +0800

    fix query/__init__.py

--- core/query/__init__.py
+++ core/query/__init__.py
@@ -1,10 +1,8 @@
 # query/__init__.py
-from .hyde import hyde_enhance, generate_hypo_docs
-from .multiquery import multiquery_enhance, generate_multi_queries
+from .hyde import hyde_enhance
+from .multiquery import multiquery_enhance
 
 __all__ = [
     "hyde_enhance",
-    "generate_hypo_docs",
     "multiquery_enhance",
-    "generate_multi_queries",
 ]
